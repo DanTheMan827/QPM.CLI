b54ce102635b994fde08c49c7a3a1b73251b540d
Daniel <790119+DanTheMan827@users.noreply.github.com>
2025-06-26 15:59:23 +0000

    Embed git version into the binary

--- build.rs
+++ build.rs
@@ -13,5 +13,33 @@ fn main() -> Result<()> {
             .emit()
             .expect("vergen failed");
     }
+
+    if std::env::var("CARGO_CFG_TARGET_OS").unwrap() == "windows" {
+        // Force rerun if Cargo.toml changes
+        println!("cargo:rerun-if-changed=Cargo.toml");
+
+        let cargo_version = env!("CARGO_PKG_VERSION"); // e.g. "1.2.3-alpha+abc123"
+
+        // Strip build metadata and pre-release (take just 1.2.3)
+        let base_version = cargo_version
+            .split(|c| c == '-' || c == '+')
+            .next()
+            .unwrap(); // "1.2.3"
+
+        let win_version = format!("{}.0", base_version); // "1.5.1.0"
+
+        let mut res = winresource::WindowsResource::new();
+        res.set_icon("icon.ico");
+        res.set("CompanyName", "Quest Package Manager");
+        res.set("FileDescription", "Quest Package Manager (QPM) is a command-line tool for managing and creating Quest mod projects. It functions as a package manager specifically designed for these projects, simplifying the process of handling mods and their dependencies.");
+        res.set("FileVersion", &win_version);
+        res.set("InternalName", "qpm");
+        res.set("OriginalFilename", "qpm.exe");
+        res.set("ProductName", "Quest Package Manager");
+        res.set("ProductVersion", &win_version);
+
+        res.compile().expect("Resource compilation failed");
+    }
+
     Ok(())
 }
